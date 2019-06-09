5214c4c9ba5b3090f5bb83cf08e16b0db030ec3d
Ilias Ismanalijev <me@il.ly>
2019-06-09 03:02:59 +0200

    config

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  productionSourceMap: false,
+  pwa: {
+    name: 'Musicplayer.io',
+    themeColor: '#0f0f0f',
+    msTileColor: '#0f0f0f',
+    appleMobileWebAppStatusBarStyle: 'black-translucent'
+  }
+}
