e1e1c937939e6ee20c8eb939dff28d93ad9984fe
Ilias Ismanalijev <me@il.ly>
2019-06-09 13:53:49 +0200

    no _

--- src/layouts/GridLayout.vue
+++ src/layouts/GridLayout.vue
@@ -23,7 +23,7 @@
 import { Component, Vue, Prop } from 'vue-property-decorator';
 import { mapState } from 'vuex'
 import { TweenLite, Back, CSSPlugin, AttrPlugin } from 'gsap/all'
-const _fixActivationTreeShake = { TweenLite, Back, CSSPlugin, AttrPlugin }
+const fixActivationTreeShake = { TweenLite, Back, CSSPlugin, AttrPlugin }
 
 const duration = 0.25
 
