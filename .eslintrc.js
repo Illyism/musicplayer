9e0d8c49b27c1b2e0017c99ec1563b15669e7ef9
Ilias Ismanalijev <me@il.ly>
2019-08-31 20:14:21 +0200

    eslint allow prefer promise reject errors, cmmt

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,7 +24,6 @@ module.exports = {
   // add your custom rules here
   rules: {
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "prefer-promise-reject-errors": "off",
     "vue/html-closing-bracket-spacing": [
       "warn",
       {
@@ -33,17 +32,17 @@ module.exports = {
         selfClosingTag: "always"
       }
     ],
-    "no-unused-vars": 0, // enabled via ts-eslint
+    "no-unused-vars": 0, // enabled via ts-eslint below
     "@typescript-eslint/no-unused-vars": ["error", {
       "vars": "all",
       "args": "after-used",
       "ignoreRestSiblings": false
     }],
-    "@typescript-eslint/no-explicit-any": 0,
-    "@typescript-eslint/explicit-function-return-type": 0,
+    "@typescript-eslint/no-explicit-any": 0, // allow "any" sometimes => makes typescript workable
+    "@typescript-eslint/explicit-function-return-type": 0, // not needed => types can be inferred
     "@typescript-eslint/camelcase": 0, // ignore => some api calls, settings use _
     "@typescript-eslint/no-var-requires": 0, // allow require('') => used by config files
-    "@typescript-eslint/no-empty-interface": 0,
-    "@typescript-eslint/no-use-before-define": ["error", { "functions": false, "classes": true }]
+    "@typescript-eslint/no-empty-interface": 0, // ignore => needed in some typings
+    "@typescript-eslint/no-use-before-define": ["error", { "functions": false, "classes": true }] // false => functions are hoisted
   }
 };
