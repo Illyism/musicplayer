bb2906b37c83aef816db4d6259e037f8fc429e5a
Ilias Ismanalijev <me@il.ly>
2019-08-31 20:08:32 +0200

    enable non null assertion rule, comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,7 +33,7 @@ module.exports = {
         selfClosingTag: "always"
       }
     ],
-    "no-unused-vars": 0,
+    "no-unused-vars": 0, // enabled via ts-eslint
     "@typescript-eslint/no-unused-vars": ["error", {
       "vars": "all",
       "args": "after-used",
@@ -41,9 +41,8 @@ module.exports = {
     }],
     "@typescript-eslint/no-explicit-any": 0,
     "@typescript-eslint/explicit-function-return-type": 0,
-    "@typescript-eslint/no-non-null-assertion": 0,
-    "@typescript-eslint/camelcase": 0,
-    "@typescript-eslint/no-var-requires": 0,
+    "@typescript-eslint/camelcase": 0, // ignore => some api calls, settings use _
+    "@typescript-eslint/no-var-requires": 0, // allow require('') => used by config files
     "@typescript-eslint/no-empty-interface": 0,
     "@typescript-eslint/no-use-before-define": ["error", { "functions": false, "classes": true }]
   }
